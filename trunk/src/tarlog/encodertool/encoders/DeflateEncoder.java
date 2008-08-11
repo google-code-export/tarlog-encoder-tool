2c09b66af7441b20b9586f2e26ed773f12e706ac
tarlog <tarlog@47e8c4f4-2954-0410-9845-e7828cb0a806>
2008-08-11 07:54:06 +0000

    add deflate

---
+++ trunk/src/tarlog/encodertool/encoders/DeflateEncoder.java
@@ -0,0 +1,48 @@
+package tarlog.encodertool.encoders;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
+
+import tarlog.encodertool.AbstractEncoder;
+import tarlog.encodertool.Utils;
+
+public class DeflateEncoder extends AbstractEncoder {
+
+    boolean nowrap = true;
+
+    @Override
+    public String getName() {
+        return "Deflate";
+    }
+
+    @Override
+    public Object encode(byte[] source) {
+
+        try {
+            Boolean input = Utils.getBooleanInput(shell, "Select compression",
+                "GZIP compatible compression", String.valueOf(nowrap));
+
+            if (input != null) {
+                nowrap = input;
+            }
+            ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
+            Deflater deflater = new Deflater(Deflater.DEFLATED, nowrap);
+            DeflaterOutputStream deflaterStream = new DeflaterOutputStream(
+                bytesOut, deflater);
+            deflaterStream.write(source);
+            deflaterStream.finish();
+            return bytesOut.toByteArray();
+        } catch (IOException e) {
+            Utils.showException(shell, e);
+            return null;
+        }
+    }
+
+    @Override
+    public Object encode(String source) {
+        return encode(source.getBytes());
+    }
+
+}
